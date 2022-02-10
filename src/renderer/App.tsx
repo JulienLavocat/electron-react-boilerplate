23beca811bb8fe18df9eeffa382aa581be0d935a
Julien Lavocat <julien.lavocat@gmail.com>
2022-02-10 20:09:49 +0100

    feat: base tabs

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,13 +1,17 @@
+import { CssBaseline } from '@mui/material';
 import { MemoryRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
-    </Router>
+    <>
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 }
--- src/renderer/components/pages/Home/index.tsx
+++ src/renderer/components/pages/Home/index.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import { createStyles, Divider, Tab, Tabs } from '@mui/material';
+import React, { useMemo, useState } from 'react';
+import { services } from '../../../utils/services';
 import Worker from '../../modules/Worker';
+import './home.scss';
 
 const ID = 'gateways/customers';
 
 export default function Home() {
-  return <Worker id={ID} />;
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const getTabLabels = () =>
+    Object.entries(services).map(([type, items]) =>
+      items.map((item) => (
+        <Tab label={type === 'gateways' ? `${item}-gateway` : item} sx={{}} />
+      ))
+    );
+
+  const tabLabels = useMemo(() => getTabLabels(), []);
+
+  return (
+    <div className="home">
+      <Tabs
+        className="tabs"
+        sx={{
+          borderRight: 1,
+          borderColor: 'divider',
+        }}
+        value={currentTab}
+        onChange={(_, value) => setCurrentTab(value)}
+        orientation="vertical"
+        variant="scrollable"
+      >
+        {...tabLabels}
+      </Tabs>
+      <Divider orientation="vertical" />
+      <Worker id={ID} />
+    </div>
+  );
 }
